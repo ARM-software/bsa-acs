297ec2648ce3bcc942003a308c7c0f744e25055e
Balaji Gontumukkala <balaji.gontumukkala@arm.com>
2025-03-18 06:24:49 +0000

    Changed Drtm ACS version to 0.6
     - Changes to make the DRTM ACS minor version to 6 from 5
       for Alpha 0.6 release
    
    Signed-off-by: Balaji Gontumukkala <balaji.gontumukkala@arm.com>

--- drtm/uefi_app/DrtmAcs.h
+++ drtm/uefi_app/DrtmAcs.h
@@ -1,5 +1,5 @@
 /** @file
- * Copyright (c) 2024, Arm Limited or its affiliates. All rights reserved.
+ * Copyright (c) 2024-2025, Arm Limited or its affiliates. All rights reserved.
  * SPDX-License-Identifier : Apache-2.0
 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,20 +18,18 @@
 #ifndef __DRTM_ACS_LEVEL_H__
 #define __DRTM_ACS_LEVEL_H__
 
+#define DRTM_ACS_MAJOR_VER      0
+#define DRTM_ACS_MINOR_VER      6
 
+#ifdef _AARCH64_BUILD_
+unsigned long __stack_chk_guard = 0xBAAAAAAD;
+unsigned long __stack_chk_fail =  0xBAAFAAAD;
+#endif
 
-  #define DRTM_ACS_MAJOR_VER      0
-  #define DRTM_ACS_MINOR_VER      5
-
-  #ifdef _AARCH64_BUILD_
-  unsigned long __stack_chk_guard = 0xBAAAAAAD;
-  unsigned long __stack_chk_fail =  0xBAAFAAAD;
-  #endif
-
-  /* Please MAKE SURE all the table sizes are 16 Bytes aligned */
-  #define PE_INFO_TBL_SZ         16384  /*Supports max 400 PEs     */
-                                        /*[24 B Each + 4 B Header] */
-  #define GIC_INFO_TBL_SZ        240000 /*Supports max 832 GIC info (GICH,CPUIF,RD,ITS,MSI,D)*/
-                                        /*[48 B Each + 32 B Header]*/
+/* Please MAKE SURE all the table sizes are 16 Bytes aligned */
+#define PE_INFO_TBL_SZ         16384  /*Supports max 400 PEs     */
+                                      /*[24 B Each + 4 B Header] */
+#define GIC_INFO_TBL_SZ        240000 /*Supports max 832 GIC info (GICH,CPUIF,RD,ITS,MSI,D)*/
+                                      /*[48 B Each + 32 B Header]*/
 
 #endif
